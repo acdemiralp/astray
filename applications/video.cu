0b1f72d900ef2d11832a9d74a5212b887813b655
acdemiralp <demiralpali@gmail.com>
2022-04-12 01:03:24 +0200

    Finished video application.

--- applications/video.cu
+++ applications/video.cu
@@ -1,37 +1,83 @@
+#include <cstddef>
 #include <cstdint>
 
 #include <astray/api.hpp>
 
-using scalar_type     = double;
-using metric_type     = ast::metrics::schwarzschild<scalar_type>;
-using geodesic_type   = ast::geodesic              <scalar_type, ast::runge_kutta_4_tableau<scalar_type>>;
-using ray_tracer_type = ast::ray_tracer            <metric_type, geodesic_type>;
+template <
+  typename scalar_type ,
+  typename metric_type = ast::metrics::kerr<scalar_type>, 
+  typename motion_type = ast::geodesic<scalar_type, ast::runge_kutta_4_tableau<scalar_type>>>
+struct settings_type
+{
+  using pixel_type           = ast::vector3<std::uint8_t>;
+  using image_type           = ast::image<pixel_type>;
+  using image_size_type      = image_type::size_type;
+  using bounds_type          = typename motion_type::bounds_type;
+  using error_evaluator_type = typename motion_type::error_evaluator_type;
+  using transform_type       = ast::transform<scalar_type>;
+  using vector_type          = typename transform_type::vector_type;
+  using projection_type      = ast::projection<scalar_type>;
+  
+  image_size_type      image_size       = {1920, 1080};
+  metric_type          metric           = {};
+  std::size_t          iterations       = 1000;
+  scalar_type          lambda_step_size = static_cast<scalar_type>(0.01);
+  scalar_type          lambda           = static_cast<scalar_type>(0);
+  bounds_type          bounds           = {};
+  error_evaluator_type error_evaluator  = {};
+  bool                 debug            = false;
+
+  vector_type          position         = vector_type(-5.0, 0.0, -5.0);
+  vector_type          rotation         = vector_type(   0,   0,    0);
+  bool                 look_at_origin   = true;
+  scalar_type          coordinate_time  = static_cast<scalar_type>(0);
+  projection_type      projection       = ast::perspective_projection<scalar_type> {ast::to_radians<scalar_type>(120), static_cast<scalar_type>(image_size[0]) / image_size[1]};
+  image_type           background_image = image_type("../data/backgrounds/checkerboard.png");
+};
+
+template <typename scalar_type, typename metric_type, typename motion_type>
+constexpr auto make_ray_tracer(const settings_type<scalar_type, metric_type, motion_type>& settings)
+{
+  auto ray_tracer = std::make_unique<ast::ray_tracer<metric_type, motion_type>>(
+    settings.image_size      ,
+    settings.metric          ,
+    settings.iterations      ,
+    settings.lambda_step_size,
+    settings.lambda          ,
+    settings.bounds          ,
+    settings.error_evaluator ,
+    settings.debug           );
+  ray_tracer->observer.transform.translation = settings.position;
+  ray_tracer->observer.transform.rotation_from_euler(settings.rotation);
+  if (settings.look_at_origin)
+    ray_tracer->observer.transform.look_at(typename ast::transform<scalar_type>::vector_type::Zero());
+  ray_tracer->observer.coordinate_time       = settings.coordinate_time;
+  ray_tracer->observer.projection            = settings.projection;
+  ray_tracer->background                     = settings.background_image;
+  return std::move(ray_tracer);
+}
 
 std::int32_t main(std::int32_t argc, char** argv)
 {
-  auto image_size = ast::vector2<std::int32_t>(160, 120);
-  
-  auto ray_tracer                           = ray_tracer_type(image_size, metric_type(), 100, 0.1);
-  ray_tracer.observer.projection            = ast::perspective_projection<scalar_type> {ast::to_radians(120.0), static_cast<scalar_type>(image_size[0]) / image_size[1]};
-  ray_tracer.observer.transform.translation = {-5.0, -5.0, 0.1};
-  ray_tracer.observer.transform.look_at({0.0, 0.0, 0.0});
-  ray_tracer.background        .load   ("../data/backgrounds/tycho_2_t5_wright_bridgman.jpg");
+  using scalar_type = float;
+
+  const auto ray_tracer = make_ray_tracer(settings_type<scalar_type, ast::metrics::schwarzschild<scalar_type>>());
 
   std::optional<ast::video> video(std::nullopt);
-  if (ray_tracer.communicator.rank() == 0)
-    video.emplace("../data/outputs/applications/video.mp4", image_size, 60);
+  if (ray_tracer->communicator.rank() == 0)
+    video.emplace("../data/outputs/applications/video.mp4", ray_tracer->partitioner.domain_size(), 60);
   
-  constexpr auto frames(100);
+  constexpr auto frames(1000);
   for (auto i = 0; i < frames; ++i)
   {
     std::cout << i << "/" << frames - 1 << "\n";
 
-    auto image = ray_tracer.render_frame();
-    if (ray_tracer.communicator.rank() == 0)
+    auto image = ray_tracer->render_frame();
+    if (ray_tracer->communicator.rank() == 0)
       video->append(image);
 
-    ray_tracer.observer.transform.translation[0] += 0.1;
-    ray_tracer.observer.transform.look_at({0.0, 0.0, 0.0});
+    ray_tracer->observer.transform.translation[0] += 0.01f;
+    ray_tracer->observer.transform.look_at({0.0, 0.0, 0.0});
   }
 
   return 0;
