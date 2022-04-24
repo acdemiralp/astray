f981145df2a3d6ac2876b013490963870a81650b
acdemiralp <demiralpali@gmail.com>
2022-04-24 07:32:45 +0200

    Updated sandbox.

--- applications/sandbox.cu
+++ applications/sandbox.cu
@@ -14,8 +14,16 @@ std::int32_t main(std::int32_t argc, char** argv)
   settings.background_image.load("../data/backgrounds/checkerboard_gray.png");
 
   const auto ray_tracer = make_ray_tracer(settings);
-  const auto image      = ray_tracer->render_frame();
+        auto image      = ray_tracer->render_frame();
+
+  auto record = ast::benchmark<float, std::milli, std::chrono::high_resolution_clock>([&]
+  {
+    image = ray_tracer->render_frame();
+  }, 1);
+  std::cout << "Mean: " << record.mean() << "\n";
+  
   if (ray_tracer->communicator().rank() == 0)
     image.save("../data/outputs/applications/sandbox.png");
+
   return 0;
 }
