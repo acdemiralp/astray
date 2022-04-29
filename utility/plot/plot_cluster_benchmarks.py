2a25c37afc9f32bc1cbab258fe05c52a6428b8a8
acdemiralp <demiralpali@gmail.com>
2022-04-29 05:00:48 +0200

    Benchmark images added. Everything complete regarding measurements.

--- utility/plot/plot_cluster_benchmarks.py
+++ utility/plot/plot_cluster_benchmarks.py
@@ -3,7 +3,7 @@ import pandas            as pd
 
 # If image size is an array of numbers, creates strong scaling plots.
 # If image size is an array of arrays , creates weak   scaling plots.
-def create_plots(file_prefix, devices, process_counts, metrics, image_size, plots):
+def create_plots(file_prefix, devices, process_counts, metrics, image_size, colors, plots):
     filtered = []
     for device in devices:
         for index, process_count in enumerate(process_counts):
@@ -20,7 +20,7 @@ def create_plots(file_prefix, devices, process_counts, metrics, image_size, plot
                     'mean'         : selection['mean'].max()})
     filtered = pd.DataFrame(filtered)
 
-    for device in devices:
+    for device_index, device in enumerate(devices):
         for index, metric in enumerate(metrics):
             selection = filtered[
                 (filtered['device'] == device) &
@@ -28,17 +28,18 @@ def create_plots(file_prefix, devices, process_counts, metrics, image_size, plot
             plots[index].set_title (metric)
             plots[index].set_xlabel("Process Count")
             plots[index].set_ylabel("Time (Milliseconds)")
-            plots[index].plot      (selection['process_count'], selection['mean'], label=device)
+            plots[index].plot      (selection['process_count'], selection['mean'], label=device, color=colors[device_index])
             plots[index].legend    ()
 
 file_prefix               = '../../data/outputs/performance/benchmark_cluster_'
-devices                   = ['cpp', 'omp', 'tbb', 'cuda']
-metrics                   = ["minkowski", "schwarzschild", "kerr", "kastor_traschen"]
+devices                   = ['cpp', 'cuda', 'omp', 'tbb']
+metrics                   = ['minkowski', 'schwarzschild', 'kerr', 'kastor_traschen']
 process_counts            = [1, 2, 4, 8, 16] # , 32, 64]
 strong_scaling_image_size = [1024, 1024]
 weak_scaling_image_sizes  = [[512, 512], [724, 724] ,[1024, 1024], [1448, 1448], [2048, 2048]] # , [2896, 2896], [4096, 4096]]
+colors                    = [(0.4, 0.4, 0.4), (0.45, 0.75, 0), (0.75, 0.225, 0.225), (0, 0.45, 0.75)]
 
 figure, plots  = plt.subplots(2,4)
-create_plots(file_prefix, devices, process_counts, metrics, strong_scaling_image_size, plots[0])
-create_plots(file_prefix, devices, process_counts, metrics, weak_scaling_image_sizes , plots[1])
+create_plots(file_prefix, devices, process_counts, metrics, strong_scaling_image_size, colors, plots[0])
+create_plots(file_prefix, devices, process_counts, metrics, weak_scaling_image_sizes , colors, plots[1])
 plt.show()
