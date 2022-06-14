a859eba8d2ccb639fb96b8951dd832af753e76ea
acdemiralp <demiralpali@gmail.com>
2022-06-14 16:03:13 +0200

    Omitted the cpp results in cluster benchmarks as it is so slow that it screws the scale of the plots. Omitted the 2896x2896 and 4096x4096 tests too.

--- utility/plot/plot_cluster_benchmarks.py
+++ utility/plot/plot_cluster_benchmarks.py
@@ -32,12 +32,12 @@ def create_plots(file_prefix, devices, process_counts, metrics, image_size, colo
             plots[index].legend    ()
 
 file_prefix               = '../../data/outputs/performance/benchmark_cluster_'
-devices                   = ['cpp', 'cuda', 'omp', 'tbb']
+devices                   = ['cuda', 'omp', 'tbb'] # 'cpp',
 metrics                   = ['minkowski', 'schwarzschild', 'kerr', 'kastor_traschen']
 process_counts            = [1, 2, 4, 8, 16] # , 32, 64]
 strong_scaling_image_size = [1024, 1024]
-weak_scaling_image_sizes  = [[512, 512], [724, 724] ,[1024, 1024], [1448, 1448], [2048, 2048]] # , [2896, 2896], [4096, 4096]]
-colors                    = [(0.4, 0.4, 0.4), (0.45, 0.75, 0), (0.75, 0.225, 0.225), (0, 0.45, 0.75)]
+weak_scaling_image_sizes  = [[512, 512], [724, 724], [1024, 1024], [1448, 1448], [2048, 2048]] # , [2896, 2896], [4096, 4096]]
+colors                    = [(0.45, 0.75, 0), (0.75, 0.225, 0.225), (0, 0.45, 0.75)] # (0.4, 0.4, 0.4), 
 
 figure, plots  = plt.subplots(2,4)
 create_plots(file_prefix, devices, process_counts, metrics, strong_scaling_image_size, colors, plots[0])
